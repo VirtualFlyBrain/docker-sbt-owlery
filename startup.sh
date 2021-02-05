618b33d0bdc7279fc8db49839844f6d06b2c81a5
hkir-dev <huseyin_kir@yahoo.com>
2021-02-05 16:52:12 +0300

    issue#26 configs and domain specific data extracted from project

--- startup.sh
+++ startup.sh
@@ -1,6 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "Loading OWLERY"
+
+## get remote configs
+echo "Sourcing remote config"
+source ${CONF_DIR}/config.env
+
+cp -v ${CONF_DIR}/application.conf /srv/conf/
+
 sed -i "s|http://www.virtualflybrain.org/owl/vfb.owl|${OWLURL}|g" /srv/conf/application.conf
 
 cat /srv/conf/application.conf
